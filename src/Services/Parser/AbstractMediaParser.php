95f311f50469eb72cdfe7b7f09bc0da395fe86fd
Halis Duraki <duraki.halis@nsoft.ba>
2017-09-15 19:44:01 +0200

    [fixed] abstract parser to intersect properly; [added] logs

--- src/Services/Parser/AbstractMediaParser.php
+++ src/Services/Parser/AbstractMediaParser.php
@@ -4,6 +4,7 @@ namespace Novosti\Services\Parser;
 
 use Novosti\Model\News;
 use Novosti\Repository\Impl\NewsRepository;
+use Novosti\Common\Logger;
 
 abstract class AbstractMediaParser
 {
@@ -23,21 +24,51 @@ abstract class AbstractMediaParser
      */
     private $repository;
 
+    /**
+     * @var $logger
+     */
+    protected $logger;
+
+    const PARSER_LOG_HANDLER = 'parser'; 
+
+    function __construct()
+    {
+        $this->logger = new Logger(self::PARSER_LOG_HANDLER);
+    }
+
+    private function isUrlInDatabase(array $results, $url)
+    {
+        foreach ($results as $db) {
+            if ($db == $url) {
+                $this->logger->write()->info("Found URL already in the database.");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     protected function save(News $article)
     {
         $this->repository = new NewsRepository;
     
-        $articles = $this->repository->getArticles(); # previous results (def: 10 past)
-        $url = []; # dataset for currently found urls
+        $pastNews = $this->repository->getArticles($article->service); # previous results, limit: 10 
+        $current = $article->url; # dataset for currently found url
 
-        foreach ($this->article as $newArticle) {
-            $urls[] = $newArticle->url;
+        $this->logger->write()->info(sprintf("Got %d past results!", count($pastNews)));
+
+        $this->logger->write()->info("Checking if article is in database ...");
+
+        if ($this->isUrlInDatabase($pastNews, $current)) {
+            $this->logger
+                ->write()
+                ->warning(sprintf("Article seems to be in the database, skipping!", count($pastNews)));
+            return true;
         }
 
-    
-
-
+        $this->logger->write()->info("Article not found, saving new one ...");
         $this->repository->saveArticle($article);
     }
 
+    abstract public function parse();
 }
--- src/Services/Parser/Impl/KlixMediaParser.php
+++ src/Services/Parser/Impl/KlixMediaParser.php
@@ -22,19 +22,31 @@ class KlixMediaParser extends AbstractMediaParser
 
     function __construct()
     {
+        parent::__construct();
+
+        $this->logger->write()->info("Creating client interface");
         $this->client = new Client;
     }
 
     public function parse()
     {
+        $this->logger->write()->info(sprintf("Requesting website with endpoint: %s", self::HTML_ENDPOINT));
         $crawl = $this->client->request('GET', self::HTML_ENDPOINT);
 
+        $this->logger->write()->info("Starting node selection and data extraction ...");
         $crawl->filter('article.kartica.srednja')->each(function($node) {
+
+            $this->logger->write()->info("Moving dataset into model group ...");
+
             $this->article = new News; 
             $this->article->comment = $node->filter('span')->text();
             $this->article->subject = $node->filter('h1')->text();
             $this->article->url     = $node->filter('a')->link()->getUri();
             $this->article->service = KlixParseCommand::PORTAL;
+            $this->article->id      = strtoupper(
+                                      str_replace(' ', '_', $this->article->comment));
+
+            $this->logger->write()->info(sprintf("Found #{ID}: %s | #{Service}: %s", $this->article->id, $this->article->service));
 
             $this->save($this->article);
         });
